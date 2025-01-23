546e0c2fad5eb05d1fd3760161ce1145545fbfea
DorianWilson <dorianthomaswilson@gmail.com>
2025-01-23 09:46:02 -0500

    Add files via upload

---
+++ Or.v
@@ -0,0 +1,11 @@
+module or_gate(
+    input [31:0] A, B,
+    output [31:0] Y
+);
+    genvar i;
+    generate
+        for (i = 0; i < 32; i = i + 1) begin : or_loop
+            assign Y[i] = A[i] | B[i]; // Bitwise OR operation
+        end
+    endgenerate
+endmodule
