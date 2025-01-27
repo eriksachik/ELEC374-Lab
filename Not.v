1712c985356d369098504406dbdcf7f58f881f6b
DorianWilson <dorianthomaswilson@gmail.com>
2025-01-27 12:14:44 -0500

    Not.v

--- Not.v
+++ Not.v
@@ -2,10 +2,10 @@ module not_gate(
     input [31:0] A,
     output reg [31:0] Y
 );
-    integer i;
-    always @* begin
-        for (i = 0; i < 32; i = i + 1) begin
-            Y[i] = ~A[i]; // Bitwise NOT operation
+    genvar i;
+    generate
+        for (i = 0; i < 32; i = i + 1) begin: loop
+            assign Y[i] = !A[i]; // Bitwise NOT operation
         end
-    end
+    endgenerate
 endmodule
