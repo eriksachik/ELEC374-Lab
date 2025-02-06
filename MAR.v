48d928aaf8e477c4be6b9eb11124fda1905cacc1
eriksachik <122634337+eriksachik@users.noreply.github.com>
2025-02-06 15:28:48 -0500

    Update MAR.v

--- MAR.v
+++ MAR.v
@@ -1,20 +1,20 @@
 `timescale 1ns/10ps 
-module MAR
-    #(parameter VAL = 0)(
+module MAR #(parameter VAL = 0)(
     input wire clk,
     input wire clr,
     input wire [31:0] dIn,
     input wire Rin,
     output reg [8:0] address
 );
-    always @(posedge clk or negedge clr)
-    begin
+
+    always @(posedge clk or negedge clr) begin
         if (clr == 0)
             address <= 0;
         else if (Rin)
-            address <= dIn[8:0];
+            address <= dIn[8:0]; // Extract the lower 9 bits without sign extension
     end
 
     initial
         address = VAL;
+
 endmodule
