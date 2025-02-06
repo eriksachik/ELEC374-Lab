a6f0679a137e9a19f53a56f3756995adccdac471
eriksachik <122634337+eriksachik@users.noreply.github.com>
2025-02-06 14:50:54 -0500

    Update multiplier_tb.v

--- multiplier_tb.v
+++ multiplier_tb.v
@@ -1,23 +1,31 @@
-
 `timescale 1ns/10ps 
-module multiplier_tb(input a, output b);
+module multiplier_tb;
 
-	wire[63:0] Rz;
-	reg[31:0] Ra;
-	reg[31:0] Rb;
-	
-	multiplier MUL(Ra, Rb, Rz);
-	
-		initial
-				begin
-			Ra = 32'hFFFFFFFF;
-			Rb = 32'h00000000;
-			#20
-			Ra = 32'h0000FF00;
-			Rb = 32'h000FFF0F;
-			#20
-			Ra = 32'FFFFFFFFF;
-			Rb = 32'h000FFF0F;
-			
-				end
+    wire [63:0] Rz;
+    reg [31:0] Ra;
+    reg [31:0] Rb;
+    
+    // Instantiate the multiplier module
+    multiplier MUL(.Mplr(Ra), .Mcnd(Rb), .Y(Rz));
+    
+    initial begin
+        // Test case 1: Multiply by 0
+        Ra = 32'hFFFFFFFF;  // Multiplier = -1
+        Rb = 32'h00000000;  // Multiplicand = 0
+        #20; // Expected Rz = 64'h0000000000000000
+        
+        // Test case 2: Multiply two random numbers
+        Ra = 32'h0000FF00;  // Multiplier = 65280
+        Rb = 32'h000FFF0F;  // Multiplicand = 1048583
+        #20; // Expected Rz = 65280 * 1048583
+        
+        // Test case 3: Multiply a negative and a positive number
+        Ra = 32'hFFFFFFFF;  // Multiplier = -1
+        Rb = 32'h000FFF0F;  // Multiplicand = 1048583
+        #20; // Expected Rz = -1048583 (64-bit negative result)
+        
+    end
 endmodule
+
+
+
