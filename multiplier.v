0ab8da458d986795268e1cf534e17c8f7eb45fc6
eriksachik <122634337+eriksachik@users.noreply.github.com>
2025-02-06 23:23:51 -0500

    Update multiplier.v

--- multiplier.v
+++ multiplier.v
@@ -1,39 +1,37 @@
-`timescale 1ns / 1ps
+`timescale 1ns/10ps
 
 module multiplier(
-    input  wire [31:0] Mplr,
-    input  wire [31:0] Mcnd,
-    output reg  [63:0] Y
+    input  wire [31:0] Mplr, input  wire [31:0] Mcnd, output reg  [63:0] Y
 );
-    reg [63:0] accum;
-    reg [63:0] multiplicand_ext;
-    reg [63:0] multiplier_ext;
-    reg [33:0] booth;        // <--- Declare here (outside procedural block)
+    reg [33:0] booth; 
+	 reg [63:0] accumulated;
+    reg [63:0] Mcnd_SE;
+    reg [63:0] Mplr_SE;
+           
 
     integer i;
 
     always @(*) begin
-        // Sign-extend A, B into 64-bit
-        multiplicand_ext = {{32{A[31]}}, A};
-        multiplier_ext    = {{32{B[31]}}, B};
+        Mplr_SE = {{32{Mplr[31]}}, Mplr};
+        Mcnd_SE    = {{32{Mcnd[31]}}, Mcnd};
 
-        accum = 64'd0;
+        accumulated = 64'd0;
 
         // Initialize booth with the lower 32 bits of the multiplier plus an extra bit
-        booth = { multiplier_ext[31:0], 1'b0 }; // 33 bits total
+        booth = { Mplr_SE[31:0], 1'b0 }; // 33 bits total
 
         // Perform Booth multiplication
         for (i = 0; i < 32; i = i + 1) begin
             case (booth[1:0])
-                2'b01: accum = accum + (multiplicand_ext << i);
-                2'b10: accum = accum - (multiplicand_ext << i);
-                default: /* no-op */;
+                2'b01: accumulated = accumulated + (Mcnd_SE << i);
+                2'b10: accumulated = accumulated - (Mcnd_SE << i);
+                default: ;
             endcase
             // Arithmetic right shift the booth value by 1
             booth = { {1{booth[33]}}, booth[33:1] };
         end
 
-        PRODUCT = accum;
+        Y = accumulated;
     end
 
 endmodule
