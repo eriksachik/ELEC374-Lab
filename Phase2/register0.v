47df16e57c3a228bf025efdedfef6e6bc28c642c
eriksachik <122634337+eriksachik@users.noreply.github.com>
2025-03-03 11:55:59 -0500

    Create register0.v

---
+++ Phase2/register0.v
@@ -0,0 +1,30 @@
+`timescale 1ns/10ps
+module register0 #(parameter DATA_WIDTH = 32, INIT = 32'h0)(
+    input clear, clock, enable, 
+    input BAout, 
+    input [DATA_WIDTH-1:0] BusMuxOut, 
+    input R0in, 
+    output reg [DATA_WIDTH-1:0] BusMuxIn 
+);
+    reg [DATA_WIDTH-1:0] q; 
+
+    initial begin
+        q <= INIT;
+    end
+
+    always @(posedge clock) begin
+        if (clear) begin
+            q <= {DATA_WIDTH{1'b0}}; 
+        end else if (R0in && enable) begin
+            q <= BusMuxOut; 
+        end
+    end
+
+    always @* begin
+      
+        for (integer i = 0; i < DATA_WIDTH; i = i + 1) begin
+            BusMuxIn[i] = q[i] & ~BAout; 
+        end
+    end
+
+endmodule
