37d65df1089d5af814f59e91c2a9951969558bf4
eriksachik <122634337+eriksachik@users.noreply.github.com>
2025-03-17 13:11:47 -0400

    Update ld_tb.v

--- Phase2/Testbenches/ld_tb.v
+++ Phase2/Testbenches/ld_tb.v
@@ -1,5 +1,5 @@
 `timescale 1ns/10ps
-module ld_tb;
+module new_datapath_tb;
 
 //	reg R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, Cout; 
 //	reg R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in;
