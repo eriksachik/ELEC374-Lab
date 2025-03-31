75414a1ecb3ef18a395402abb55315621919388a
Dorian Wilson <dorianthomaswilson@gmail.com>
2025-03-31 13:25:13 -0400

    Update ControlUnit_tb.v

--- Phase3/ControlUnit_tb.v
+++ Phase3/ControlUnit_tb.v
@@ -1,14 +1,14 @@
 `timescale 1ns/10ps
 module control_unit_tb();
 		reg clk,reset,stop;
-		reg [31:0] inport_data;
-		wire [31:0] outport_data;
+	reg [31:0] IN_data;
+	wire [31:0] OUT;
 		wire run;
 	initial begin
 		clk=0;
 		reset=0;
 		stop=0;
-		inport_data=0;
+		IN_data=0;
 		
 	end
 	always #10 clk=~clk;
@@ -22,8 +22,8 @@ module control_unit_tb();
 
 datapath datapath(
 	.clk(clk),.reset(reset),.stop(stop),
-	.inport_data(inport_data),
-	.outport_data(outport_data),
+	.inport_data(IN_data),
+	.outport_data(OUT),
 	.run(run)
 );
 endmodule
